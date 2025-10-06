cccebec160ae9ea38f190253236d08e44dc9837e
Kleopatra Jäger <151923676+kleospace@users.noreply.github.com>
2025-10-06 22:31:36 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,46 +7,23 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use App\Models\{Stylebook, Article, Comment};
+use App\Models\{Stylebook, Article};
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     protected $fillable = ['name','email','password'];
     protected $hidden = ['password','remember_token'];
 
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    protected function casts(): array {
+        return ['email_verified_at' => 'datetime', 'password' => 'hashed'];
     }
 
-    public function initials(): string
-    {
-        return Str::of($this->name)
-            ->explode(' ')
-            ->take(2)
-            ->map(fn ($word) => Str::substr($word, 0, 1))
-            ->implode('');
+    public function initials(): string {
+        return Str::of($this->name)->explode(' ')->take(2)->map(fn($w)=>Str::substr($w,0,1))->implode('');
     }
 
-    // Relations
-    public function stylebooks()
-    {
-        return $this->hasMany(Stylebook::class, 'user_id');
-    }
-
-    public function articles()
-    {
-        return $this->hasMany(Article::class, 'author_id');
-    }
-
-    public function comments()
-    {
-        return $this->hasMany(Comment::class);
-    }
+    public function stylebooks() { return $this->hasMany(Stylebook::class, 'user_id'); }
+    public function articles()   { return $this->hasMany(Article::class, 'author_id'); }
 }
