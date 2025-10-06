e8d9e9317545e24370940ead619af57093a00ba3
Kleopatra Jäger <151923676+kleospace@users.noreply.github.com>
2025-10-06 11:37:25 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,26 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StylebookController;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+// Öffentliche Landing-Pages
+Route::get('/stylebooks', [StylebookController::class, 'landing'])->name('stylebooks.landing');
+Route::get('/articles',   [ArticleController::class, 'landing'])->name('articles.landing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Beispiel: spätere CRUD-Routen
+    // Route::resource('stylebooks', StylebookController::class);
+    // Route::resource('stylebooks.articles', ArticleController::class)->shallow();
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
