3c7f2705c0cdf594ec6ec31a4ce136dc9c13bf60
Kleopatra Jäger <151923676+kleospace@users.noreply.github.com>
2025-10-06 12:18:30 +0200

    Update ArticleFactory.php

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,22 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
- */
 class ArticleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'author_id' => User::factory(),
+            'is_public' => $this->faker->boolean(80),
+            'title'     => $this->faker->sentence(4),
+            'content'   => $this->faker->paragraphs(2, true),
         ];
     }
 }
