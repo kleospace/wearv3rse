eeaf6a62bc521e788b265512f4ec8cd01314eea8
Kleopatra Jäger <151923676+kleospace@users.noreply.github.com>
2025-10-06 11:30:04 +0200

    Update StylebookFactory.php

--- database/factories/StylebookFactory.php
+++ database/factories/StylebookFactory.php
@@ -2,22 +2,17 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Stylebook>
- */
 class StylebookFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'title' => $this->faker->unique()->sentence(3),
+            'description' => $this->faker->optional()->paragraph(),
         ];
     }
 }
