450799b18434779487d6566ea450392eba14497c
Kleopatra Jäger <151923676+kleospace@users.noreply.github.com>
2025-10-06 21:12:09 +0200

    Update 2025_10_06_190254_create_comments_table.php

--- database/migrations/2025_10_06_190254_create_comments_table.php
+++ database/migrations/2025_10_06_190254_create_comments_table.php
@@ -4,22 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('stylebook_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('article_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->text('text');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('comments');
